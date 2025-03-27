754aae048b2577a1a3dd7dbbb58e8d7251fbc69f
Beats <makesbeats11@gmail.com>
2025-03-26 22:36:45 -0500

    added zk plugin to nvim

--- dot_config/nvim/lua/lazy-plugins.lua
+++ dot_config/nvim/lua/lazy-plugins.lua
@@ -45,7 +45,9 @@ require("lazy").setup({
 
 	require("kickstart/plugins/neorg"),
 
-  require("kickstart/plugins/supermaven"),
+	require("kickstart/plugins/supermaven"),
+
+	require("kickstart/plugins/zk"),
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
